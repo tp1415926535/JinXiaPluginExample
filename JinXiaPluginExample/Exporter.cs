7c819ec7e8813497f4db628b596908db57064dba
tp1415926535 <iamhermit@163.com>
2024-12-15 18:00:48 +0800

    添加项目文件。

---
+++ JinXiaPluginExample/Exporter.cs
@@ -0,0 +1,78 @@
+
+using Plugger;
+using System.Composition;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+
+namespace JinXiaPluginExample
+{
+    [Export(typeof(IPlugger))]
+    public class Exporter : IPlugger
+    {
+        public string Id { get; set; } = "示例插件";
+        public string Name { get; set; } = "示例插件";
+        public string Author { get; set; } = "官方_纵码过河山";
+        public string Description { get; set; } = "仅用于提供示例";
+        public PlugInType PlugInType { get; set; } = PlugInType.Other;
+        public ImageSource Icon { get { return new BitmapImage(new Uri("/JinXiaPluginExample;component/示例.png", UriKind.Relative)); } }
+        public Version Version { get; set; } = Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// 窗口
+        /// </summary>
+        private Window1 window { get; set; }
+
+        /// <summary>
+        /// 打开
+        /// </summary>
+        public void Open()
+        {
+            if (window == null)
+            {
+                window = new Window1();
+                window.Closed += Window_Closed;
+            }
+            window.Show();
+            window.Activate();
+        }
+        /// <summary>
+        /// 窗体关闭事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            window = null;
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        public void Close()
+        {
+            if (window != null)
+                window.Close();
+        }
+
+        ///// <summary>
+        ///// 插件是否有设置
+        ///// </summary>
+        //public bool HasSetting { get; set; } = true;
+
+        ///// <summary>
+        ///// 显示插件内设置
+        ///// </summary>
+        //public void ShowSetting()
+        //{
+        //    if (window == null)
+        //    {
+        //        window = new Window1();
+        //        window.Closed += Window_Closed;
+        //    }
+        //    window.ShowSetting();
+        //}
+    }
+
+}
---
+++ Plugger/IPlugger.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace Plugger
+{
+    /// <summary>
+    /// 插件接口
+    /// </summary>
+    public interface IPlugger
+    {
+        /// <summary>
+        /// 插件唯一Id
+        /// </summary>
+        string Id { get; set; }
+        /// <summary>
+        /// 插件名
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// 作者名
+        /// </summary>
+        string Author { get; set; }
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        Version Version { get; set; }
+        /// <summary>
+        /// 介绍
+        /// </summary>
+        string Description => string.Empty;
+        /// <summary>
+        /// 相关链接
+        /// </summary>
+        string Link => string.Empty;
+        /// <summary>
+        /// 图标
+        /// </summary>
+        ImageSource Icon => null;
+        /// <summary>
+        /// 分类
+        /// </summary>
+        PlugInType PlugInType => PlugInType.Other;
+        /// <summary>
+        /// 打开方法
+        /// </summary>
+        /// <returns></returns>
+        void Open();
+        /// <summary>
+        /// 关闭方法
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// 是否有设置页面
+        /// </summary>
+        bool HasSetting => false;
+
+        /// <summary>
+        /// 显示设置方法
+        /// </summary>
+        void ShowSetting() { }
+    }
+
+    /// <summary>
+    /// 插件类型
+    /// </summary>
+    public enum PlugInType
+    {
+        /// <summary>
+        /// 未设置
+        /// </summary>
+        [Description("未设置")]
+        None,
+        /// <summary>
+        /// 系统
+        /// </summary>
+        [Description("系统")]
+        System,
+        /// <summary>
+        /// 媒体
+        /// </summary>
+        [Description("媒体")]
+        Media,
+        /// <summary>
+        /// 网络
+        /// </summary>
+        [Description("网络")]
+        Network,
+        /// <summary>
+        /// 生活
+        /// </summary>
+        [Description("生活")]
+        Life,
+        /// <summary>
+        /// 自动化
+        /// </summary>
+        [Description("自动")]
+        Automation,
+        /// <summary>
+        /// 教育
+        /// </summary>
+        [Description("教育")]
+        Education,
+        /// <summary>
+        /// 商业
+        /// </summary>
+        [Description("商业")]
+        Business,
+        /// <summary>
+        /// 开发
+        /// </summary>
+        [Description("开发")]
+        Development,
+        /// <summary>
+        /// 安全
+        /// </summary>
+        [Description("安全")]
+        Security,
+        /// <summary>
+        /// 游戏
+        /// </summary>
+        [Description("游戏")]
+        Gaming,
+        /// <summary>
+        /// 数据
+        /// </summary>
+        [Description("数据")]
+        Database,
+        /// <summary>
+        /// 其他
+        /// </summary>
+        [Description("其他")]
+        Other,
+    }
+}
